f7d910ba65afb6afab33139d98848fccfe020dc9
Bruno Ferraz <ferraz.b@outlook.com>
2021-09-30 16:38:41 -0300

    Add simple strings

---
+++ strings.py
@@ -0,0 +1,17 @@
+person_name = "Eric"
+print(f"Hello {person_name}, would you like to learn some Python today?")
+
+print(f"{person_name.lower()} {person_name.upper()} {person_name.title()}")
+
+print("Albert Einstein: \"A person who never made a mistake never tried anything new\"")
+print('Albert Einstein: "A person who never made a mistake never tried anything new\"')
+
+famous_person = "Albert Einstein"
+message = "\"A person who never made a mistake never tried anything new\""
+print(f"{famous_person}: {message}")
+
+person_name = " Eric "
+print(person_name+"test")
+print(person_name.rstrip()+"test")
+print(person_name.lstrip()+"test")
+print(person_name.strip()+"test")
